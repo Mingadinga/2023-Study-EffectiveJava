680fa454b974935cfe0670832d4d4f2faf678fb0
mingadinga <minpearl0826@gmail.com>
2023-05-29 16:04:06 +0900

    Item18. 상속보다는 컴포지션을 사용하라

---
+++ src/main/java/org/example/item18/callback/BobFunction.java
@@ -0,0 +1,20 @@
+package org.example.item18.callback;
+
+public class BobFunction implements FunctionToCall {
+
+    public final Service service;
+
+    public BobFunction(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void call() {
+        System.out.println("밥을 먹을까..");
+    }
+
+    @Override
+    public void run() {
+        this.service.run(this);
+    }
+}
---
+++ src/main/java/org/example/item18/callback/FunctionToCall.java
@@ -0,0 +1,6 @@
+package org.example.item18.callback;
+
+public interface FunctionToCall {
+    public void call();
+    public void run();
+}
